18d3f39c4502a640964f8a8756facd7e80054804
Gustavo Meneghetti <gustavo96ma@gmail.com>
2024-05-17 20:22:33 -0300

    consumo banco de dados

--- 2_BIMESTRE/AULA02/lib/widgets/mensagens_chat.dart
+++ 2_BIMESTRE/AULA02/lib/widgets/mensagens_chat.dart
@@ -1,21 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'mensagem.dart';
 
 class MensagensChat extends StatelessWidget {
-  const MensagensChat({super.key});
+  final String chatId;
+  const MensagensChat({super.key, required this.chatId});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return const Mensagem();
-          },
-        ),
-      ),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('salas')
+          .doc(chatId)
+          .collection('mensagens')
+          .orderBy('criadoEm', descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('Nenhuma mensagem enviada!'),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Algum erro desconhecido ocorreu'),
+          );
+        }
+
+        final mensagensCarregadas = snapshot.data!.docs;
+
+        return Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: mensagensCarregadas.length,
+              itemBuilder: (context, index) {
+                return Mensagem(
+                  conteudoMensagem: mensagensCarregadas[index]['conteudo'],
+                  nomeUsuario: mensagensCarregadas[index]['email'],
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
